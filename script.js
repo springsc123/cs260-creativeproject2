6cb26600f7286dd8c6aa5d4357c9edaf7bf381cb
springsc123 <springscullen@yahoo.com>
2019-10-11 01:57:33 +0000

    updated format

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*global fetch*/
-function init(){
+function init() {
     let dropdown = document.getElementById("currencyType");
     const url = "https://api.exchangerate-api.com/v4/latest/USD";
     fetch(url)
@@ -8,39 +8,32 @@ function init(){
         }).then(function(json) {
             console.log(json);
             var text = "";
-            for (var i in json.rates){
-                text +=  "<option value=\"" + i + "\">" + i + "</option>"
+            for (var i in json.rates) {
+                text += "<option value=\"" + i + "\">" + i + "</option>"
             }
             dropdown.innerHTML = text;
         });
 }
 
-window.onload = function() {
-document.getElementById("moneySubmit").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const currency = document.getElementById("currencyType").value;
-    const value = document.getElementById("moneyInput").value;
-    console.log("value is " + currency);
-    console.log("moneyInput is " + value);
+var el = document.getElementById('moneySubmit');
+if (el) {
+    document.getElementById("moneySubmit").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const currency = document.getElementById("currencyType").value;
+        const value = document.getElementById("moneyInput").value;
+        console.log("value is " + currency);
+        console.log("moneyInput is " + value);
 
-    if (currency === "")
-        return;
-    console.log(currency);
-    
-    const url = "https://api.exchangerate-api.com/v4/latest/" + currency;
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            console.log(json);
+        if (currency === "")
+            return;
+        console.log(currency);
 
-            
-        });
-
-
-});
-
-
-
+        const url = "https://api.exchangerate-api.com/v4/latest/" + currency;
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                console.log(json);
+            });
+    });
 }
-

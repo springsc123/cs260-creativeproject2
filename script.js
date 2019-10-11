8424bf6d2bdb8ac9c2b4ad2f65091261368b1474
hooperxander@gmail.com <hooperxander@gmail.com>
2019-10-11 15:32:25 +0000

    added second selector and correct rounding

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*global fetch*/
 function init() {
-    let dropdown = document.getElementById("currencyType");
+    let dropdownTo = document.getElementById("currencyTypeTo");
+    let dropdownFrom = document.getElementById("currencyTypeFrom");
     const url = "https://api.exchangerate-api.com/v4/latest/USD";
     fetch(url)
         .then(function(response) {
@@ -11,31 +12,33 @@ function init() {
             for (var i in json.rates) {
                 text += "<option value=\"" + i + "\">" + i + "</option>"
             }
-            dropdown.innerHTML = text;
+            dropdownTo.innerHTML = text;
+            dropdownFrom.innerHTML = text;
         });
 }
 
 function submitCurrency() {
-        const currency = document.getElementById("currencyType").value;
+        const currencyFrom = document.getElementById("currencyTypeFrom").value;
+        const currencyTo = document.getElementById("currencyTypeTo").value;
         const value = document.getElementById("moneyInput").value;
-        console.log("value is " + currency);
+        console.log("value is " + currencyFrom);
         console.log("moneyInput is " + value);
 
-        if (currency === "")
+        if (currencyFrom === "")
             return;
-        console.log(currency);
+        console.log(currencyFrom);
 
-        const url = "https://api.exchangerate-api.com/v4/latest/" + currency;
+        const url = "https://api.exchangerate-api.com/v4/latest/" + currencyFrom;
         fetch(url)
             .then(function(response) {
                 return response.json();
             }).then(function(json) {
                 console.log(json);
                 var text = "";
-                var result = json.rates["USD"]
+                var result = json.rates[currencyTo]
                 console.log("RESULT: " + result)
-                console.log("value / result: " + (value / result))
+                console.log("value * result: " + (value * result))
                 
-                document.getElementById("currencyResult").innerHTML = (value / result);
+                document.getElementById("currencyResult").innerHTML = ((value * result) + .005).toFixed(2);
             });
 }
